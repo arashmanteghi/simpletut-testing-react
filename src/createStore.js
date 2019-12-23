0fd6302b285a3fc782306e658eefed8386c30772
Arash Manteghi <arash.manteghi@gmail.com>
2019-12-24 00:46:24 +0330

    Add Redux to our app

---
+++ src/createStore.js
@@ -0,0 +1,10 @@
+ import { createStore, applyMiddleware } from 'redux';
+ import ReduxThunk from 'redux-thunk';
+
+ import RootReducer from './reducers';
+
+ export const middleWares = [ReduxThunk];
+
+ export const createStoreWithMiddleware = applyMiddleware(...middleWares)(createStore);
+
+ export const store = createStore(RootReducer);

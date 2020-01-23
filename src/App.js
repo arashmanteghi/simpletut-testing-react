bf1fab66562bb29d62325fc027b1f2c7b30eba51
Arash Manteghi <arash.manteghi@gmail.com>
2020-01-23 12:48:16 +0330

    test methods of a class component

--- src/App.js
+++ src/App.js
@@ -13,23 +13,45 @@ const tempArr = [{
   email: 'arash.manteghi@gmail.com',
   age: 31,
   onlineStatus: true,
-}]
+}];
+
+const initialState = {
+  hideBtn: false
+};
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      ...initialState,
+    };
   }
 
   fetch = () => {
     this.props.fetchPosts();
-  }
+    this.exampleMethod_updatesState()
+  };
+
+  exampleMethod_updatesState = () => {
+    const { hideBtn } = this.state;
+    this.setState({
+      hideBtn: !hideBtn
+    });
+  };
+
+  exampleMethod_returnsAValue = (number) => {
+    return number + 1;
+  };
 
   render() {
     const { posts } = this.props;
+    const { hideBtn } = this.state;
+
     const configButton = {
       buttonText: 'Get posts',
       emitEvent: this.fetch
-    }
+    };
+
     return (
       <div className="App" data-test="appComponent">
         <Header />
@@ -39,7 +61,11 @@ class App extends Component {
             desc="Click the button to render posts"
             tempArr={tempArr}
           />
-          <SharedButton {...configButton} />
+
+          {!hideBtn &&
+            <SharedButton {...configButton} />
+          }
+
           {posts.length > 0 && 
             <div>
               {posts.map((post, index) => {
